9161f89064790cb0132c9a5e08a92d38e0d66fd9
Vinícius Roque <viniciusfr6@gmail.com>
2023-02-24 21:28:47 -0300

    feat(): begin of organizing in layered architecture

---
+++ src/controllers/userController.js
@@ -0,0 +1,12 @@
+app.post('/sign-up', (req, res) => {
+  const { username, avatar } = req.body;
+
+  if (!username || !avatar) {
+    res.status(400).send('Todos os campos são obrigatórios!');
+    return;
+  }
+
+  usuarios.push({ username, avatar });
+
+  res.status(200).send('OK deu tudo certo');
+});
